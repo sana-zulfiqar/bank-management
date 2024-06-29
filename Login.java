542f016cda61fe80cac8eeb484824f25d5c7037f
sana-zulfiqar <Sanaazulfiqar003@gmail.com>
2024-06-29 05:12:53 -0800

    Add files via upload

---
+++ Login.java
@@ -0,0 +1,17 @@
+package com.company;
+
+public class Login {
+String username;
+String password;
+Login(){
+username="sana ahmed";
+password="1234";
+}
+boolean validate( String username, String password) {
+if(this.username.equalsIgnoreCase(username) &&
+this.password.equals(password))
+return true;
+else
+return false;
+}
+}
